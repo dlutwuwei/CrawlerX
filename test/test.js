8bc2f43d183437b16a9da363f7e204ffd1e6363e
dlutwuwei <dlutwuwei@sina.com>
2014-05-09 14:10:59 +0800

    Initial commit 修改url处理

--- test/test.js
+++ test/test.js
@@ -3,10 +3,10 @@
  */
 var crawler = require('../lib/crawler');
 
-crawler.addStrategy(/.elong./,function(err, $, body, url, resp){
+crawler.addStrategy(/http:\/\/cnodejs.org./,function(err, $, body, url, resp){
 	  //console.log(body);
     console.log($("title").text(),url);
 });
-crawler.request("http://flight.elong.com/",{decode:null,deep:3,concurrency:5}, function(err, $, body,resp){
+crawler.request("http://cnodejs.org/",{decode:"utf-8",deep:3,concurrency:3}, function(err, $, body,resp){
   $.spider();
 });
