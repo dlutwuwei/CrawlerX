0c895f8003737ad64aee07d0678af278244dd1cc
dlutwuwei <dlutwuwei@sina.com>
2014-05-12 18:29:35 +0800

    Initial commit 增加export index

---
+++ index.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Administrator on 14-5-12.
+ */
+var crawler = require('./lib/crawler');
+exports.crawler = crawler;
--- test/test.js
+++ test/test.js
@@ -3,10 +3,10 @@
  */
 var crawler = require('../lib/crawler');
 
-crawler.addStrategy(/http:\/\/cnodejs.org./,function(err, $, body, url, resp){
+crawler.addStrategy('*',function(err, $, body, url, resp){
 	  //console.log(body);
     console.log($("title").text(),url);
 });
-crawler.request("http://cnodejs.org/",{decode:"utf-8",deep:3,concurrency:3}, function(err, $, body,resp){
+crawler.request("http://www.elong.com",{decode:"utf-8",deep:3,concurrency:3}, function(err, $, body,resp){
   $.spider();
 });
