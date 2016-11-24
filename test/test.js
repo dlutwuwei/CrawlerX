2ae3f40c6499f97e48a7d5bf5eda95925b4f7779
dlutwuwei <dlutwuwei@qq.com>
2016-11-24 23:13:28 +0800

    test url

--- test/test.js
+++ test/test.js
@@ -7,6 +7,6 @@ crawler.addStrategy('*',function(err, $, body, url, resp){
 	  //console.log(body);
     console.log($("img").attr('src'));
 });
-crawler.request("http://c6.d5j.biz/htm_data/16/1611/2135730.html",{decode:"utf-8",deep:3,concurrency:3}, function(err, $, body,resp){
+crawler.request("http://baidu.com/",{decode:"utf-8",deep:3,concurrency:3}, function(err, $, body,resp){
   $.spider();
 });
